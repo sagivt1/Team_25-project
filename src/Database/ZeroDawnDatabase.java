f04d3bf3a744561f83b3dfa2f6b568114dc45a6b
sagivt1 <58602415+sagivt1@users.noreply.github.com>
2021-04-03 23:25:05 +0300

    fix 3

---
+++ src/Database/ZeroDawnDatabase.java
@@ -0,0 +1,42 @@
+package Database;
+
+import java.sql.*;
+
+public class ZeroDawnDatabase {
+
+    Connection conn = null;
+
+
+    /*
+    * Creating new connection to the database and return conn for future use
+    * of the database
+    *
+    * before use this class change the password to your localhost password
+    *  */
+    public Connection GetDb(){
+
+        try {
+
+            String url = "jdbc:mysql://localhost/zero_dawn?useSSL=false";
+            String user = "root";
+            String password = "*******";//Edit to your mysql password
+
+            conn = DriverManager.getConnection(url, user, password);
+
+            return conn;
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            return null;
+        }
+
+    }
+
+
+
+
+
+}

c8f458a3940f14832120d675dcac92ce68c6edde
amit9530 <amitbracha55@gmail.com>
2021-04-05 17:31:02 +0300

    BSPM2021T25-27 BSPM2021T25-52 BSPM2021T25-54 BSPM2021T25-68 BSPM2021T25-71 BSPM2021T25-82 BSPM2021T25-84 BSPM2021T25-72 BSPM2021T25-76 BSPM2021T25-17 BSPM2021T25-18 BSPM2021T25-4 data validation

--- src/MainPage.java
+++ src/MainPage.java
@@ -1,6 +1,13 @@
 import Database.ZeroDawnDatabase;
 import java.sql.*;
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.Scanner;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.*;
+import java.util.*;
 
 public class MainPage {
 
@@ -20,51 +27,164 @@ public class MainPage {
         if(mSelect == 1)
         {
             sign_up(con);
-            //צריך לפנות פה לתפריט של המחלקה הספציפית שהבן אדם נרשם
-            System.out.println("sign-up select is: " + mSelect);
+            //we need to change the sign-up function from void to return
         }
         if(mSelect == 2)
         {
             login(con);
-            //צריך לפנות פה לתפריט של המחלקה הספציפית שהבן אדם התחבר
-            System.out.println("login select is: " + mSelect);
+            //we need to change the login function from void to return
         }
         if(mSelect == 3)
         {
             System.exit(0);
         }
-
     }
 
     public static void sign_up(Connection con) throws SQLException {
+
         while (true) {
             try {
                 Scanner scanObj = new Scanner(System.in);
-                PreparedStatement stmt = con.prepareStatement("Insert into users values (?,?,?,?,?,?)");
-                System.out.print("Enter user id: ");
-                String ID = scanObj.nextLine();
-                stmt.setString(1, ID);
+                PreparedStatement stmt = con.prepareStatement("Insert into users values (?,?,?,?,?,?,?)");
 
-                System.out.print("Enter user first name: ");
-                String fName = scanObj.nextLine();
-                stmt.setString(2, fName);
+                while (true) {
+                    System.out.print("Enter user id: ");
+                    String ID = scanObj.nextLine();
+                    if (!ID.isEmpty()) {
+                        stmt.setString(1, ID);
+                        break;
+                    }
+                    else {
+                        System.out.println("you didn't insert an ID, please try again..");
+                    }
+                }
 
-                System.out.print("Enter user last name: ");
-                String lName = scanObj.nextLine();
-                stmt.setString(3, lName);
+                while (true) {
+                    System.out.print("Enter user password: ");
+                    String pass = scanObj.nextLine();
+                    if (!pass.isEmpty()) {
+                        stmt.setString(2, pass);
+                        break;
+                    }
+                    else {
+                        System.out.println("you didn't insert a password, please try again..");
+                    }
+                }
 
+                while (true) {
+                    System.out.print("Enter user first name: ");
+                    String fName = scanObj.nextLine();
+                    if (!fName.isEmpty()) {
+                        stmt.setString(3, fName);
+                        break;
+                    }
+                    else {
+                        System.out.println("you didn't insert your first name, please try again..");
+                    }
+                }
+
+                while (true) {
+                    System.out.print("Enter user last name: ");
+                    String lName = scanObj.nextLine();
+                    if (!lName.isEmpty()) {
+                        stmt.setString(4, lName);
+                        break;
+                    }
+                    else {
+                        System.out.println("you didn't insert your last name, please try again..");
+                    }
+                }
+
+
+                /* Date working without testing of values
                 System.out.print("Enter user birth date (example: 1999-02-23 ): ");
                 String bDate = scanObj.nextLine();
+                stmt.setString(5, bDate);
+                */
 
-                stmt.setString(4, bDate);
 
-                System.out.print("Enter user password: ");
-                String pass = scanObj.nextLine();
-                stmt.setString(5, pass);
+                /* Date test 1 not working, smart way
+                while (true) {
+                    //Scanner dscan = new Scanner(System.in);
+                    Date bt = new Date();
+                    DateFormat dt = new SimpleDateFormat("yyyy-MM-dd");
+                    try {
+                        System.out.print("Enter user birth date (example: 1999-02-23 ): ");
+                        String bDate = scanObj.nextLine();
+                        bDate = dt.format(bt);
+                        stmt.setString(5, bDate);
+                        break;
+                    } catch (Exception dex) {
+                        System.out.println("Date is Invalid" + dex.getMessage());
+                    }
+                }
+                */
 
-                System.out.print("Enter user type: ");
-                String type = scanObj.nextLine();
-                stmt.setString(6, type);
+
+                /* Date test 2 not working, not smart way
+                while (true) {
+                    System.out.print("Enter user birth year (yyyy): ");
+                    String yDate = scanObj.nextLine();
+                    int check = Integer.valueOf(yDate);
+                    if(!yDate.isEmpty() && check > 1900 && check < 2015)
+                    {
+                        while ()
+                    }
+                    else {
+                        System.out.println("you didn't insert a year or the year is Invalid, please try again..");
+                    }
+                }
+                */
+
+
+
+
+                while (true) {
+                    String regex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}";
+                    Pattern pattern = Pattern.compile(regex);
+                    System.out.print("Enter user email: ");
+                    String email = scanObj.nextLine();
+                    Matcher match = pattern.matcher(email);
+                    if (!email.isEmpty() && match.matches()) {
+                        stmt.setString(6, email);
+                        break;
+                    }
+                    else {
+                        System.out.println("you didn't insert an email or the email is Invalid, please try again..");
+                    }
+                }
+
+                while (true) {
+                    String newLine = System.getProperty("line.separator");
+                    System.out.printf("Enter 1 for student" + newLine + "Enter 2 for parent" + newLine + "Enter 3 for counselor" + newLine + "Enter Type: ");
+                    char type = scanObj.next().charAt(0);
+                    while (type != '1' && type != '2' && type != '3') {
+                        System.out.println("Wrong input, try again..");
+                        System.out.printf("Enter 1 for student" + newLine + "Enter 2 for parent" + newLine + "Enter 3 for counselor" + newLine + "Enter Type: ");
+                        type = scanObj.next().charAt(0);
+                    }
+                    if (type == '3') {
+                        Scanner pScan = new Scanner(System.in);
+                        System.out.println("Please enter password for counselor creation: ");
+                        String tPass = pScan.nextLine();
+                        while (!tPass.equals("1111")) {
+                            System.out.println("Wrong password, try again..");
+                            System.out.println("To cancel the action and exit the system enter 1 in the password");
+                            System.out.println("Please enter password for counselor creation: ");
+                            tPass = pScan.nextLine();
+                            if(tPass.equals("1")) {
+                                System.exit(0);
+                            }
+                        }
+                    }
+                    if (type != ' ') {
+                        stmt.setString(7, String.valueOf(type));
+                        break;
+                    }
+                    else {
+                        System.out.println("you didn't insert your type, please try again..");
+                    }
+                }
 
                 stmt.executeUpdate();
                 break;
@@ -79,9 +199,11 @@ public class MainPage {
     }
 
     public static void login(Connection con) throws SQLException {
+
         Scanner scanID = new Scanner(System.in);
         Scanner scanPass = new Scanner(System.in);
         PreparedStatement stmt = con.prepareStatement("Insert into users values (?,?,?,?,?,?)");
+        
         while (true) {
             try {
                 System.out.print("Enter your id: ");
@@ -102,5 +224,4 @@ public class MainPage {
             }
         }
     }
-
 }
